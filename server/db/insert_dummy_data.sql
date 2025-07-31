401a21c39bdfa5d250ca11a764ec829868ea1bdc
김채연 <kcy211211@naver.com>
2025-07-31 14:58:01 +0900

    change yaw to degree

--- server/db/insert_dummy_data.sql
+++ server/db/insert_dummy_data.sql
@@ -45,17 +45,15 @@ INSERT INTO `log_type` (`type`, `description`) VALUES
 -- 부서 데이터 (department 테이블)
 -- 번호: 0=CT, 1=초음파, 2=X-ray, 3=대장암, 4=위암, 5=폐암, 6=뇌종양, 7=유방암
 INSERT INTO `department` (`department_id`, `department_name`, `location_x`, `location_y`, `yaw`) VALUES
-(0, 'CT 검사실', -5.79, -1.88, 1.57),        -- CT (90도 = 1.57 라디안)
-(1, '초음파 검사실', -4.9, -1.96, 1.57),     -- Echography 
-(2, 'X-ray 검사실', -5.69, 4.34, 3.14),     -- X-ray (180도 = 3.14 라디안)
+(0, 'CT 검사실', -5.79, -1.88, 90.0),        -- CT (90도 = 1.57 라디안)
+(1, '초음파 검사실', -4.9, -1.96, 90.0),     -- Echography 
+(2, 'X-ray 검사실', -5.69, 4.34, 180.0),     -- X-ray (180도 = 3.14 라디안)
 (3, '대장암 센터', 0.93, -2.3, 0.0),        -- Colon Cancer
 (4, '위암 센터', 3.84, -2.3, 0.0),          -- Stomach Cancer
 (5, '폐암 센터', 5.32, -2.27, 0.0),         -- Lung Cancer
-(6, '뇌종양 센터', 5.97, 1.46, 3.14),       -- Brain Tumor (180도)
-(7, '유방암 센터', 7.64, 1.63, 3.14),       -- Breast Cancer (180도)
-(8, '병원 로비', 9.53, -1.76, 1.57),        -- Lobby Station (90도)
-(9, '통로 A', 0.09, 4.0, 3.14),             -- Gateway A (180도)
-(10, '통로 B', -2.6, 4.18, 0.0);           -- Gateway B (0도)
+(6, '뇌종양 센터', 5.97, 1.46, 180.0),       -- Brain Tumor (180도)
+(7, '유방암 센터', 7.64, 1.63, 180.0);       -- Breast Cancer (180도)
+
 
 -- 예약 데이터 (reservations 테이블 - patient_id, reservation_date만)
 INSERT INTO `reservations` (`patient_id`, `reservation_date`) VALUES
@@ -78,19 +76,3 @@ INSERT INTO `robot_log` (`robot_id`, `patient_id`, `dttm`, `orig`, `dest`, `type
 (3, 10021002, '2025-01-25 10:20:00', 8, 0, 'moving_by_patient'),  -- 이환자: 로비 → CT  
 (3, 10021002, '2025-01-25 11:00:00', 0, 3, 'moving_by_patient'),  -- 이환자: CT → 대장암센터
 (3, 10031003, '2025-01-25 13:50:00', 8, 1, 'moving_by_patient');  -- 박환자: 로비 → 초음파
-
-<<<<<<< HEAD
-INSERT INTO `log_type` (`type`, `description`) VALUES
-('moving_by_patient', '환자 이동'),
-('moving_by_robot', '로봇 이동'),
-('moving_by_admin', '관리자 이동'),
-('moving_by_unknown', '본인 인증 없이 이동'),
-('return_by_patient', '환자 반환'),
-('return_by_robot', '로봇 반환'),
-('return_by_admin', '관리자 반환'),
-('return_by_unknown', '본인 인증 없이 반환'),
-('assigned_by_unknown', '사용자 호출'),
-('interruption', '장애물 감지 중단');
-
-=======
->>>>>>> adcb64b8fee4f617c0b00126157e936ecd635c82
